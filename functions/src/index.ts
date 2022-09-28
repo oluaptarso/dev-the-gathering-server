e0002f2a7f53dadbac07ae4b7a3cf17f66f453dd
paulohyst <oluaptarsso@gmail.com>
2022-09-28 13:07:02 -0300

    Added GRAPHQL_PLAYGROUND configuration to .env

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,13 +1,16 @@
 import * as functions from 'firebase-functions';
-import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
+import { ApolloServerPluginLandingPageGraphQLPlayground, ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 import { ApolloServer } from 'apollo-server-cloud-functions';
 import { schema } from './schema';
 import { context } from './context';
 
+const introspection = process.env.GRAPHQL_PLAYGROUND === 'true';
+
 const apolloServer = new ApolloServer({
   schema,
   context,
-  plugins: [ApolloServerPluginLandingPageGraphQLPlayground],
+  introspection,
+  plugins: [introspection ? ApolloServerPluginLandingPageGraphQLPlayground : ApolloServerPluginLandingPageLocalDefault],
 });
 
 exports.graphql = functions.https.onRequest(apolloServer.createHandler());
