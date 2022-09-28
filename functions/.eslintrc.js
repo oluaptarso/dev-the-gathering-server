ea75132262158bdb5f1dc97d78e0459fffdd6f8d
paulohyst <oluaptarsso@gmail.com>
2022-09-28 10:17:37 -0300

    Initial commit

---
+++ functions/.eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'google',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: ['tsconfig.json', 'tsconfig.dev.json'],
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  ignorePatterns: [
+    '/lib/**/*', // Ignore built files.
+  ],
+  plugins: ['@typescript-eslint', 'import'],
+  rules: {
+    'quotes': ['error', 'single'],
+    'indent': ['error', 2],
+    'object-curly-spacing': ['error', 'always'],
+    'max-len': ['error', { code: 180 }],
+    'import/no-unresolved': 0,
+    'require-jsdoc': 0,
+    'valid-jsdoc': 'off',
+    'new-cap': 0,
+    'linebreak-style': 0,
+    'import/namespace': 0,
+  },
+};
